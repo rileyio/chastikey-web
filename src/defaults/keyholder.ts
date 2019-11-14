18dab728375358a5c5ad89ad3b9ca4fd5263fc8e
Emma <emma@tdm.io>
2019-11-14 01:26:15 -0400

    Project has moved - Removing some stuff

--- src/defaults/keyholder.ts
+++
@@ -1,3 +0,0 @@
-export const $KeyholderView = {
-  locks: []
-}
--- src/defaults/session.ts
+++
@@ -1,7 +0,0 @@
-import { KieraCachedSession } from '@/objects/session'
-
-export const $DefaultSession = {
-  cached: new KieraCachedSession({}),
-  isAuthenticated: false,
-  isLoaded: false
-}
